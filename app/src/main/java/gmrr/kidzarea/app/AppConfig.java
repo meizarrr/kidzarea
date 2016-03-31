3a0734154799385c6e6df2806e99d2c6f620d6f2
Muhammad Rifqi Zuliansyah <rifqized96@gmail.com>
2016-03-31 19:40:23 +0700

    Login dan Database

---
+++ app/src/main/java/gmrr/kidzarea/app/AppConfig.java
@@ -0,0 +1,12 @@
+package gmrr.kidzarea.app;
+
+/**
+ * Created by Rifqi Zuliansyah on 31/03/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.166/kidzarea-server/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.43.166/kidzarea-server/register.php";
+}
